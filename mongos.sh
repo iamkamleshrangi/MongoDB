3a879adab0cdc5f3a266110679a66d3a6f3385ad
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2018-03-20 13:31:18 +0530

    Update mongos.sh

--- mongos.sh
+++ mongos.sh
@@ -3,10 +3,10 @@
 #Title: Script to install MongoDB and Create sharding with support for 3.6 on ubuntu 16.04
 
 echo "Installing MongoDB 3.6 Latest one"
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update
-apt-get install -y mongodb-org
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 systemctl start mongod
 systemctl status mongod
 
